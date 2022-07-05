45884ddfa1222c0e79f3e62211c287a08b817317
josh <whonotcare@gmail.com>
2022-07-05 18:56:54 +0800

    init commit

---
+++ src/token/token.ts
@@ -0,0 +1,35 @@
+export enum TokenType {
+    Illegal = "illegal",
+    EOF = "EOF",
+    Identifier = "identifier",
+    Int = "int",
+    Assign = "=",
+    Plus = "+",
+    Comma = ",",
+    Semicolon = ";",
+    L_Paren = "(",
+    R_Paren = ")",
+    L_Brace = "{",
+    R_Brace = "}",
+    Function = "function",
+    Var = "Var"
+}
+
+class Token {
+    constructor(
+        public type: TokenType,
+        public literal: string,
+    ) {}
+}
+
+class TokenList {
+    constructor(
+        public tokens: Token[] = [],
+        public current: number = -1,
+    ) {}
+
+    nextToken() {
+        this.current += 1
+        return this.tokens[this.current]
+    }
+}
